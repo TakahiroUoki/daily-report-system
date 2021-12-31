3fc6d35b165a901429856f76311988b621b23c53
takahiro.uoki <takahiro.uoki.hiryu0604@gmail.com>
2021-12-31 19:05:50 +0900

    エラーを修正

--- src/main/java/constants/JpaConst.java
+++ src/main/java/constants/JpaConst.java
@@ -51,7 +51,7 @@ public interface JpaConst {
     String REP_COL_PROGRESS = "progress"; //進捗
     String REP_COL_CREATED_AT = "created_at"; // 登録日時
     String REP_COL_UPDATED_AT = "updated_at"; // 更新日時
-    String REP_COL_CLIID = "cliId"; // 顧客ID
+    String REP_COL_CLIID = "client_Id"; // 顧客ID
 
     // Entity名
     String ENTITY_EMP = "employee"; // 従業員
